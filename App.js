968c1f551a909ed74a6136d58c53e981adbec50b
Argie <argie.b@flowerstore.ph>
2024-09-16 14:26:18 +0800

    initial setup

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View className="login-main min-h-screen flex justify-center items-center">
+      <Text className="text-green-500 text-xl font-bold">hello from React native!</Text>
+    </View>
+  )
+}
+
+export default App
